08908f44884e64d896e673d79dc36d2487aae930
weicool <weicool@gmail.com>
2012-05-11 00:38:13 -0700

    Initial commit. Let's do this thing.

---
+++ thatsaprotip/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thatsaprotip::Application.config.secret_token = '08e72a4ea86db4a48d874cd2dd467fd6a06e5c01ba97ed270c63afa91ce7ed6e275cb0aa5f7198bdeb916083604cb2e478ba6ec8f6a0d4e1dae067edc57bdcfa'
