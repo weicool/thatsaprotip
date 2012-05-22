299e3c6a6566213e75757d4c8c8defda0a11f9eb
weicool <weicool@gmail.com>
2012-05-22 02:15:10 -0700

    Attempting to fix deployment issues

--- thatsaprotip/config/deploy.rb
+++ thatsaprotip/config/deploy.rb
@@ -9,21 +9,15 @@ set :scm, :git
 set :deploy_to, '/home/weicool/thatsaprotip/public'
 set :environment, 'production'
 set :branch, 'master'
-set :deploy_via, :remote_cache
 
 server 'thatsaprotip.com', :app, :web, :db, :primary => true
 set :user, 'weicool'
 set :use_sudo, false
-
-after 'deploy:setup', 'deploy:create_release_dir'
 
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-  task :create_release_dir, :except => {:no_release => true} do
-    run "mkdir -p #{fetch :releases_path}"
   end
 end
