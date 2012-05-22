da54cce8d78e2d5d2d53252f2edc734dbfc4c0b4
weicool <weicool@gmail.com>
2012-05-22 03:13:54 -0700

    Y U NO WORK

--- thatsaprotip/config/deploy.rb
+++ thatsaprotip/config/deploy.rb
@@ -4,13 +4,43 @@ default_run_options[:pty] = true  # Must be set for the password prompt from git
 ssh_options[:forward_agent] = true
 set :repository,  'git@github.com:weicool/thatsaprotip.git'
 set :scm, :git
-set :deploy_to, '/home/weicool/thatsaprotip/public'
+set :deploy_to, '/home/weicool/thatsaprotip/public/rails'
 set :environment, 'production'
 set :branch, 'master'
 
 server 'thatsaprotip.com', :app, :web, :db, :primary => true
 set :user, 'weicool'
 set :use_sudo, false
+
+set :deploy_subdir, 'thatsaprotip'
+
+require 'capistrano/recipes/deploy/strategy/remote_cache'
+
+class RemoteCacheSubdir < Capistrano::Deploy::Strategy::RemoteCache
+
+  private
+
+  def repository_cache_subdir
+    if configuration[:deploy_subdir] then
+      File.join(repository_cache, configuration[:deploy_subdir])
+    else
+      repository_cache
+    end
+  end
+
+  def copy_repository_cache
+    logger.trace "copying the cached version to #{configuration[:release_path]}"
+    if copy_exclude.empty?
+      run "cp -RPp #{repository_cache_subdir} #{configuration[:release_path]} && #{mark}"
+    else
+      exclusions = copy_exclude.map { |e| "--exclude=\"#{e}\"" }.join(' ')
+      run "rsync -lrpt #{exclusions} #{repository_cache_subdir}/* #{configuration[:release_path]} && #{mark}"
+    end
+  end
+
+end
+
+set :strategy, RemoteCacheSubdir.new(self)
 
 namespace :deploy do
   task :start do ; end
