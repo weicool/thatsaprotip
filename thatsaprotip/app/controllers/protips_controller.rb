8b60b35074421e7391d2504579829ce2c77c946f
weicool <weicool@gmail.com>
2012-06-10 22:18:07 -0700

    Publishing

--- thatsaprotip/app/controllers/protips_controller.rb
+++ thatsaprotip/app/controllers/protips_controller.rb
@@ -12,5 +12,19 @@ class ProtipsController < ApplicationController
   end
 
   def create
+    tags = (params[:protip][:tags] || '').split.map do |tag|
+      Tag.find_or_create_by_tag(tag.downcase)
+    end
+    protip_text = params[:protip][:protip].strip
+    if tags.empty? || protip_text.empty?
+      redirect_to '/'
+      return
+    end
+
+    protip = Protip.new :protip => protip_text,
+      :user => User.viewer
+    protip.tags = tags
+    protip.save
+    redirect_to '/'
   end
 end
